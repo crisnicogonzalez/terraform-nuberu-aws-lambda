7139f5f05c52bc0a30461bd578ec5a1d46f85248
crisgonzalezallaria <143832930+crisgonzalezallaria@users.noreply.github.com>
2024-05-01 12:14:00 -0300

    [improvement] - get current accountId and region by data, use dummy image to init image

--- data.tf
+++ data.tf
@@ -2,4 +2,6 @@ data "aws_default_tags" "this" {}
 
 data "aws_region" "current" {}
 
+data "aws_caller_identity" "current" {}
+
 
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "this" {
   function_name = var.function_name
   role          = aws_iam_role.iam_for_lambda.arn
   package_type  = "Image"
-  image_uri     = "278495751288.dkr.ecr.us-east-1.amazonaws.com/pepe:latest-2"
+  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/lambda-dummy:latest"
   description   = var.description
   timeout       = var.timeout_in_seconds
   memory_size   = var.memory_size_in_mb
